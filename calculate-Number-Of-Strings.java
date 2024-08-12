9b8d7516499440a5c2ab7c48b25f9a271015614a
Junaid Aslam <junaidaslam.muet@gmail.com>
2024-08-12 20:51:36 +0500

    Create calculate-Number-Of-Strings.java

---
+++ calculate-Number-Of-Strings.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "abc"; // example string
+        System.out.println("Number of strings: " + countStrings(str));
+    }
+
+    public static int countStrings(String str) {
+        // Remove duplicate characters and store in a set
+        Set<Character> set = new HashSet<>();
+        for (char c : str.toCharArray()) {
+            set.add(c);
+        }
+
+        // Calculate the sum of factorials from 1 to the size of the set
+        int n = set.size();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            int factorial = 1;
+            for (int j = 2; j <= i; j++) {
+                factorial *= j;
+            }
+            sum += factorial;
+        }
+
+        return sum;
+    }
+}
